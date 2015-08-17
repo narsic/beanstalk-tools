76b0f0e8f5c18038fc267d245c3c879321397c7d
Onur Yalazı <onur.yalazi@8digits.com>
2015-08-17 19:59:57 +0000

    formatted queue stats output

--- bin/beanstalk-queue-stats.rb
+++ bin/beanstalk-queue-stats.rb
@@ -12,20 +12,28 @@ def delta(v)
   v.to_i > 0 ? "+#{v}" : v.to_s
 end
 
-previously={}
 
+ts=B.stats_tube tubes[0]
+ts.delete('name')
+titles = "%10.10s " % ""
+ts.keys.sort.each do |k|
+  titles += "%10.10s " % k.gsub(/-/,'').gsub(/current/, '').gsub(/jobs/, '')
+end
+
+previously={}
 loop do
   puts "#{Time.now.to_s}"
+  puts titles
   tubes.each do |tube|
-    puts "#{tube}"
+    print "%10.10s " % tube
     ts=B.stats_tube tube
     ts.delete('name')
     deltas = previously[tube] || Hash.new(0)
     ts.keys.sort.each do |k|
-      puts " - #{k} = #{ts[k]} (#{delta(ts[k] - deltas[k])})"
+      print "%10.10s " % "#{ts[k]}"  #{delta(ts[k] - deltas[k])}"
     end
+    puts ""
     previously[tube] = ts
   end
-  puts "------------------"
-  sleep 10
+  sleep 5 
 end
